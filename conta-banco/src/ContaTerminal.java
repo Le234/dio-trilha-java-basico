9e07204e493c51f217784231b0e8cc8a505ca1dd
LEANDRO MELO OLIVEIRA <leandromelodia@gmail.com>
2024-08-29 19:14:12 -0300

    codigo concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        int numero;
+        String nomeCliente;
+        String agencia = "";
+        double saldo = 237.48; 
+           
+           System.out.println("Por favor, digite o número da Agência !");
+                agencia = new Scanner(System.in) .nextLine();
+            System.out.println("Por favor, digite o número da Conta !");
+                numero = new Scanner(System.in) .nextInt();
+            System.out.println("Por favor, digite seu Nome !");
+                nomeCliente = new Scanner(System.in). nextLine();
+                System.out.println("Olá " + nomeCliente + "\n"
+                +"obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + "\n"
+                +"conta: "+ numero + "\n"
+                +"e seu saldo " + saldo + " já está disponível para saque ");
+     }
 }
